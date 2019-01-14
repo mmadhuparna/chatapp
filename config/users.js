3b08978f510bdb19898de65fd0151fcf4f0012b1
mmadhuparna <mmadhuparna@yahoo.com>
2019-01-14 16:18:06 +0530

    commit

---
+++ config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Rick',
+    lastName: 'Gabriel',
+    statusText: 'I am the leader!',
+    image: 'users/rick.jpg'
+  },
+  {
+    name: 'Krish',
+    lastName: 'Dixon',
+    statusText: 'I like smashing Walkers.',
+    image: 'users/daryl.jpg'
+  },
+  {
+    name: 'Carolina',
+    lastName: 'Mathew',
+    statusText: 'Don\'t mess with me!',
+    image: 'users/carol.jpg'
+  },
+  {
+    name: 'Spiderman',
+    lastName: '',
+    statusText: 'Believe in versatality.',
+    image: 'users/negan.jpeg'
+  }
+]
+
